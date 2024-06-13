757d1f7114590d700bc5398774f01651863a8571
paolo-depa <paolo.depasquale@suse.com>
2024-06-13 10:21:42 +0200

    crm_attribute_loop:  - feature: added stracing capabilty  - feature: added the capability to compress the logs  - rework: logging reviewed

--- README.md
+++ README.md
@@ -9,13 +9,15 @@ This project contains a shell script that continuously runs the `crm_attribute` 
 - The lifetime of the attribute can be set to either "forever" or "reboot".
 - The script can be configured to use the -G option with the `crm_attribute` command.
 - The script can be configured to use the -VVVVVV option for verbose output with the `crm_attribute` command.
+- The script can be configured to use strace to trace the system calls and signals made by the `crm_attribute` command.
+- The script can be configured to compress the log file using a specified compression utility.
 - The output can be logged to a file. If no file is specified, the output will be printed to the console.
 
 ## Usage
 
 1. Replace the `attributeName` variable with your actual attribute name.
-2. Set the `isPermanent`, `isGet`, and `isVerbose` variables as needed.
-3. If you want to log the output to a file, set the `logFile` variable to the path of your log file.
+2. Set the `isPermanent`, `isGet`, `isVerbose`, `isStrace`, and `isLogCompressed` variables as needed.
+3. If you want to log the output to a file, set the `logDir` variable to the path of your log file.
 4. Run the script.
 
 ## Example
--- crmattribute_loop.sh
+++ crmattribute_loop.sh
@@ -1,23 +1,43 @@
 #!/bin/bash
-# This script runs the crm_attribute command in a loop, printing the start time, return value, and end time for each iteration.
-# The nodeName is set to the hostname of the current machine.
-# The attributeName should be replaced with the actual attribute name.
-# If isPermanent is set 1, the lifetime is set to "forever", "reboot" otherwise.
-# If isGet is set to 1, the -G option is added to the crm_attribute command.
-# If isVerbose is set to 1, the -VVVVVV option is added to the crm_attribute command.
-# The output is logged to a file specified by the logFile variable.
-# If logFile is empty, the output will not be redirected and will only be printed to the console.
+# This script continuously executes the crm_attribute command, logging the start time, return value, and end time for each iteration.
+# The nodeName is automatically set to the hostname of the current machine.
+# The attributeName should be replaced with the actual attribute name you want to use.
+# If isPermanent is set to 1, the attribute's lifetime is set to "forever". If it's set to 0, the lifetime is set to "reboot".
+# If isGet is set to 1, the -G option is added to the crm_attribute command, which makes the command get the current value of the attribute.
+# If isVerbose is set to 1, the -VVVVVV option is added to the crm_attribute command, which makes the command output verbose information.
+# If isStrace is set to 1, the strace command is used to trace the system calls and signals made by the crm_attribute command.
+# If isLogCompressed is set to 1, the log file is compressed using the compression utility specified by the compressionUtil variable.
+# The output is logged to a file located in the directory specified by the logDir variable. The filename is generated based on the current date and time.
+# If logDir is not a valid directory, it will be created.
+
 
 nodeName=$(hostname)
 attributeName="hana_wq1_roles" # replace with your actual attribute name
 isPermanent=0
 isGet=1
 isVerbose=1
+isStrace=1
+isLogCompressed=1
+compressionUtil="gzip"
+compressionUtilParam="-1" # Balance compression and overhead on CPU
 scriptName=$(basename $0)
-logFile="/var/log/crmattribute_loop.log"
-#logFile=""
+logDir="/tmp/crmattribute_loop"
 
 command="crm_attribute -N $nodeName -n $attributeName"
+
+if [ $isLogCompressed -eq 1 ]
+then
+  if ! command -v $compressionUtil &> /dev/null
+  then
+    echo "$scriptName - Error: Compression utility '$compressionUtil' not found." >&2
+    exit -1
+  fi
+fi
+
+if [ ! -d $logDir ]
+then
+  mkdir -p $logDir
+fi
 
 if [ $isPermanent -eq 1 ]
 then
@@ -38,16 +58,25 @@ fi
 
 while true
 do
-  
-  if [ -n "$logFile" ]
+
+  START_DATE=$(date +'%Y-%m-%dT%H:%M:%S.%3N')
+  logFile="$logDir/$START_DATE.log"
+  echo "$scriptName - Creating $logFile..."
+
+  if [ $isStrace -eq 1 ]
   then
-    echo "$scriptName - $(date +'%Y-%m-%d %H:%M:%S.%3N') - Executing $command" >> $logFile
-    $command >> $logFile 2>&1
-    echo "$scriptName - $(date +'%Y-%m-%d %H:%M:%S.%3N'): Return value: $?" >> $logFile
+    strace -ff -tt -s 256 -T -o $logFile $command 1>/dev/null
   else
-    echo "$scriptName - $(date +'%Y-%m-%d %H:%M:%S.%3N') - Executing $command"
-    $command
-    echo "$scriptName - $(date +'%Y-%m-%d %H:%M:%S.%3N'): Return value: $?"
+    echo "$scriptName - $START_DATE - Executing $command" >> $logFile
+    $command >> $logFile 2>&1
+    END_DATE=$(date +'%Y-%m-%dT%H:%M:%S.%3N')
+    echo "$scriptName - $END_DATE: Return value: $?" >> $logFile
   fi
+
+  if [ $isLogCompressed -eq 1 ]
+  then
+    $compressionUtil $compressionUtilParam $logFile*
+  fi
+ 
   sleep 1
 done
